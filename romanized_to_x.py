4492831a3db86cbf5e315875158cffdcd1ee4c25
Yeok-c <yeokch@gmail.com>
2024-01-10 23:16:30 +0800

    updated readme

--- romanized_to_x.py
+++ romanized_to_x.py
@@ -2,12 +2,15 @@ import js2py
 
 js2py.translate_file("./romanized_to_hangul.js", "./r2h.py")
 from r2h import *
-print(r2h.performConversion("OH Dae han"))
+print(r2h.performConversion("Kim Yeon a"))
 
 js2py.translate_file("./romanized_to_cyrillic.js", "./r2c.py")
 from r2c import *
-print(r2c.performConversion("KANAYEVA Alua"))
+print(r2c.performConversion("Garry Kimovich Kasparov"))
 
 ## Output
-# 옿 대 한
-# КАНАЫЕВА Алуа
+# Kim Yeon-a    -> 김연아 
+# (ground truth == 김연아)
+
+# Garry Kimovich Kasparov -> Гарры Кимович Каспаров 
+#           (ground truth == Гарри Кимович Каспаров)
